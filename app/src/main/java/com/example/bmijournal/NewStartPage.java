9781a1d03190b1b4d2745ec924d0a88a91970181
sirteteux <60591186+sirteteux@users.noreply.github.com>
2021-05-11 23:39:25 +0800

    UI/UX
    Added registration, login, transition and BMI data page. Not fully completed yet.

---
+++ app/src/main/java/com/example/bmijournal/NewStartPage.java
@@ -0,0 +1,14 @@
+package com.example.dawjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewStartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_start_page);
+    }
+}
